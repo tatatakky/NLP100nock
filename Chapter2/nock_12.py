0f4c1d0a75996f6c2fad23054027f6a30f7deaa3
tatatakky <s1240232@gmail.com>
2018-02-10 23:55:02 +0900

    [add] 3rd commit

--- Chapter2/nock_12.py
+++ Chapter2/nock_12.py
@@ -14,3 +14,8 @@ for line in open('hightemp.txt','r'):
 
 fout1.close()
 fout2.close()
+
+# with open("hightemp.txt") as f1, open("col1.txt","w") as f2, open("col1.txt","w") as f3:
+#     cols=list(zip(*[row.split("\t") for row in f1]))
+#     f2.write("\n".join(cols[0]))
+#     f3.write("\n".join(cols[1]))
