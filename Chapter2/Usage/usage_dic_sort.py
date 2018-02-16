63d3febd56332d8bfe4b85863126227b8d02b768
tatatakky <s1240232@gmail.com>
2018-02-16 13:36:45 +0900

    [add] 1st commit

--- Chapter2/Usage/usage_dic_sort.py
+++
@@ -1,9 +0,0 @@
-# usr/bin/env python
-#-*- coding:utf-8 -*-
-
-key=['2','1','3','4','5']
-value=['bb','aa','cc','dd','ee']
-dic=dict(zip(key,value))
-# sorted(ディクショナリの中身一覧, key=lambda x:ソートしたい部分)
-sort_data = sorted(dic.items(),key=lambda x:x[1])
-print(sort_data)
